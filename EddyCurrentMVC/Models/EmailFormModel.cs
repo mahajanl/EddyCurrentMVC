069c16d1c18d7f371cbcc2f92ea7c9b9a155d0c4
mahajanl <lakshamima@gmail.com>
2018-01-19 11:04:11 -0500

    adding email model

---
+++ EddyCurrentMVC/Models/EmailFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EddyCurrentMVC.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+
+        [Required, Display(Name = "Your email address"), EmailAddress]
+        public string FromEmail { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
