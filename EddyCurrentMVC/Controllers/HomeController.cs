545269e8164f4468906ef2f18756401eca65b505
mahajanl <lakshamima@gmail.com>
2018-01-23 11:48:25 -0500

    changed contact to a simple mailto for speed's sake and instafunctionality

--- EddyCurrentMVC/Controllers/HomeController.cs
+++ EddyCurrentMVC/Controllers/HomeController.cs
@@ -31,42 +31,42 @@ namespace EddyCurrentMVC.Controllers
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Contact(EmailFormModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
-                var message = new MailMessage();
-                message.To.Add(new MailAddress("bablu.ratnaparkhi@gmail.com"));  // replace with valid value 
-                message.From = new MailAddress("sender@outlook.com");  // replace with valid value
-                message.Subject = "Your email subject";
-                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
-                message.IsBodyHtml = true;
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Contact(EmailFormModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+        //        var message = new MailMessage();
+        //        message.To.Add(new MailAddress("bablu.ratnaparkhi@gmail.com"));  // replace with valid value 
+        //        message.From = new MailAddress("sender@outlook.com");  // replace with valid value
+        //        message.Subject = "Your email subject";
+        //        message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+        //        message.IsBodyHtml = true;
 
-                using (var smtp = new SmtpClient())
-                {
-                    var credential = new NetworkCredential
-                    {
-                        UserName = "user@outlook.com",  // replace with valid value
-                        Password = "password"  // replace with valid value
-                    };
-                    smtp.Credentials = credential;
-                    smtp.Host = "smtp-mail.outlook.com";
-                    smtp.Port = 587;
-                    smtp.EnableSsl = true;
-                    await smtp.SendMailAsync(message);
-                    return RedirectToAction("Sent");
-                }
-            }
-            return View(model);
-        }
+        //        using (var smtp = new SmtpClient())
+        //        {
+        //            var credential = new NetworkCredential
+        //            {
+        //                UserName = "user@outlook.com",  // replace with valid value
+        //                Password = "password"  // replace with valid value
+        //            };
+        //            smtp.Credentials = credential;
+        //            smtp.Host = "smtp-mail.outlook.com";
+        //            smtp.Port = 587;
+        //            smtp.EnableSsl = true;
+        //            await smtp.SendMailAsync(message);
+        //            return RedirectToAction("Sent");
+        //        }
+        //    }
+        //    return View(model);
+        //}
 
-        public ActionResult MessageSent()
-        {
-            return View();
-        }
+        //public ActionResult MessageSent()
+        //{
+        //    return View();
+        //}
 
         public ActionResult GetQuote()
         {
